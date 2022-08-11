c7e90c55f3ba4d5f9dc1c471e8a4ca3479d726fc
Kendall Eberly <ke05739@georgiasouthern.edu>
2022-08-10 23:31:39 -0400

    commit

---
+++ src/idk.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class idk {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the name of your ticket");
+        System.out.println(createTicket(input.nextLine()));
+
+    }
+
+    public static String createTicket(String yas) {
+       int [] = new array[10];
+    }
+
+
+}
+
+
